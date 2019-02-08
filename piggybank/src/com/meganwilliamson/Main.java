f44055ec98b6ddf02d4951e2906e1e1eabf1382d
gooseandmegander <meganawilliamson@gmail.com>
2019-02-08 08:44:48 -0800

    Initial commit

---
+++ piggybank/src/com/meganwilliamson/Main.java
@@ -0,0 +1,14 @@
+package com.meganwilliamson;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        // create piggybank collection
+        // add currency to it
+
+        // print amount of each currency in bank
+
+        // print total bank value
+    }
+}
